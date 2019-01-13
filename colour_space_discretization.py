3054e5062838d8580c6736d3f3bacd7f9fe853da
Aris Spyrou <p15132@students.cs.unipi.gr>
2019-01-13 19:08:07 +0200

    fixes

--- colour_space_discretization.py
+++ colour_space_discretization.py
@@ -24,23 +24,24 @@ del a, b, im, ab_ind
 # median = np.median(ab, axis=0, overwrite_input=True)
 # ab = ab[ab[:, 0].argsort()]
 # Points at which the pixels are divided
-bin_list = [[0, ab.shape[0]]]
+bins = [[0, ab.shape[0]]]
 # Channel is 0 for alpha and 1 for beta
 channel = 0
 
-for x in range(5):
+for x in range(4):
     for y in range(2**x):
-        median = np.median(ab[bin_list[2**x - 1 + y][0]:bin_list[2**x - 1 + y][1]], axis=0)
-        # TODO a[a[:, 0].argsort()[2:5]]
-        ab[bin_list[2**x - 1 + y][0]:bin_list[2**x - 1 + y][1]] = ab[ab[bin_list[2**x - 1 + y][0]:bin_list[2**x - 1 + y][1], channel].argsort()]
-        for i in range(bin_list[2**x - 1 + y][0], ab.shape[0]):
+        x_ind = 2**x - 1 + y
+        median = np.median(ab[bins[x_ind][0]:bins[x_ind][1]], axis=0)
+        # ab[bins[x_ind][0]:bins[x_ind][1]] = ab[ab[bins[x_ind][0]:bins[x_ind][1], channel].argsort()]
+        ab[bins[x_ind][0]:bins[x_ind][1]] = ab[np.add(ab[bins[x_ind][0]:bins[x_ind][1], channel].argsort(), np.full(bins[x_ind][1]-bins[x_ind][0], bins[x_ind][0]))]
+        for i in range(bins[x_ind][0], bins[x_ind][1]):
             if ab[i][channel] >= median[channel]:
-                bin_list.append([bin_list[2**x - 1 + y][0], i])
-                bin_list.append([i, bin_list[2**x - 1 + y][1]])
+                bins.append([bins[x_ind][0], i])
+                bins.append([i, bins[x_ind][1]])
                 break
 
     if channel == 0:
         channel = 1
     else:
         channel = 0
-print(len(bin_list))
+print(len(bins))
