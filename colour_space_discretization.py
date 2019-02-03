a913ee1d1f2e52a8488b7d85e73019fa955050fb
Aris Spyrou <p15132@students.cs.unipi.gr>
2019-02-03 20:14:08 +0200

    finalized colour discretization

--- colour_space_discretization.py
+++ colour_space_discretization.py
@@ -1,3 +1,5 @@
+import glob
+
 import cv2
 import numpy as np
 
@@ -5,45 +7,61 @@ import numpy as np
 cv2.setUseOptimized(True)
 cv2.setNumThreads(cv2.getNumberOfCPUs())
 
-# Read the image and convert it to Lab
-im = cv2.imread('castle.jpg')
-# TODO resize if needed
-im = cv2.cvtColor(im, cv2.COLOR_BGR2LAB)
-(l, a, b) = cv2.split(im)
-del l
 
-# Make a list with a, b coordinates
-ab = np.zeros((a.shape[0]*a.shape[1], 2), int)
-ab_ind = 0
-for i in range(a.shape[0]):
-    for j in range(a.shape[1]):
-        ab[ab_ind] = [a[i, j], b[i, j]]
-        ab_ind += 1
-del a, b, im, ab_ind
+def discrete_colors(path='images/*.jpg'):
+    filelist = glob.glob(path)
+    imgs = []
+    for path in filelist:
+        tmp = cv2.imread(path)
+        if tmp.shape[1] >= 400:
+            scale = tmp.shape[1] // 300
+        else:
+            scale = 1
+        tmp = cv2.resize(tmp, (int(tmp.shape[1] / scale), int(tmp.shape[0] / scale)))
+        tmp = cv2.cvtColor(tmp, cv2.COLOR_BGR2Lab)
+        imgs.append(tmp)
+    del tmp, filelist
 
-# median = np.median(ab, axis=0, overwrite_input=True)
-# ab = ab[ab[:, 0].argsort()]
-# Points at which the pixels are divided
-bins = [[0, ab.shape[0]]]
-# Channel is 0 for alpha and 1 for beta
-channel = 0
+    ab = []
+    for im in imgs:
+        (l, a, b) = cv2.split(im)
+        # Make a list with a, b coordinates
 
-for x in range(4):
-    for y in range(2**x):
-        x_ind = 2**x - 1 + y
-        median = np.median(ab[bins[x_ind][0]:bins[x_ind][1]], axis=0)
-        # ab[bins[x_ind][0]:bins[x_ind][1]] = ab[ab[bins[x_ind][0]:bins[x_ind][1], channel].argsort()]
-        ab[bins[x_ind][0]:bins[x_ind][1]] = ab[np.add(ab[bins[x_ind][0]:bins[x_ind][1], channel].argsort(), np.full(bins[x_ind][1]-bins[x_ind][0], bins[x_ind][0]))]
-        for i in range(bins[x_ind][0], bins[x_ind][1]):
-            if ab[i][channel] >= median[channel]:
-                bins.append([bins[x_ind][0], i])
-                bins.append([i, bins[x_ind][1]])
-                break
+        for i in range(a.shape[0]):
+            for j in range(a.shape[1]):
+                ab.append([a[i, j], b[i, j]])
 
-    if channel == 0:
-        channel = 1
-    else:
-        channel = 0
-print(len(bins))
+    del l, a, b
+    ab = np.asarray(ab)
+    # median = np.median(ab, axis=0, overwrite_input=True)
+    # ab = ab[ab[:, 0].argsort()]
+    # Points at which the pixels are divided
+    bins = [[0, ab.shape[0] - 1]]
+    # Channel is 0 for alpha and 1 for beta
+    channel = 0
 
-# TODO not final
+    for x in range(4):
+        for y in range(2 ** x):
+            x_idx = 2 ** x - 1 + y
+            median = np.median(ab[bins[x_idx][0]:bins[x_idx][1]], axis=0)
+            ab[bins[x_idx][0]:bins[x_idx][1]] = ab[np.add(ab[bins[x_idx][0]:bins[x_idx][1], channel].argsort(),
+                                                          np.full(bins[x_idx][1] - bins[x_idx][0], bins[x_idx][0]))]
+            for i in range(bins[x_idx][0], bins[x_idx][1]):
+                if ab[i][channel] >= median[channel]:
+                    bins.append([bins[x_idx][0], i])
+                    bins.append([i, bins[x_idx][1]])
+                    break
+
+        if channel == 0:
+            channel = 1
+        else:
+            channel = 0
+
+    for i in range(2 ** 4 - 1):
+        bins.pop(0)
+
+    colors = []
+    for rn in bins:
+        colors.append(np.median(ab[rn[0]:rn[1]], axis=0))
+
+    return colors
